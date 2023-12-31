61cc2f64007f577d8a321d1719d4c11c16a2da2b
Chris <1158505+ccomparini@users.noreply.github.com>
2023-12-31 16:48:20 +0000

    virtual-joystick decent styling, moves from events

--- virtual-joystick.js
+++ virtual-joystick.js
@@ -5,10 +5,14 @@
        <script src="virtual-joystick.js"></script>
 
        <virtual-joystick id="joystick-p1"> </virtual-joystick>
+
+    Styling: XXX fill in
  */
 
 
 class VirtualJoystickElement extends HTMLElement {
+
+  #subClasses = [ ];   // this really needed?
 
   constructor() { super(); }
 
@@ -22,47 +26,37 @@ class VirtualJoystickElement extends HTMLElement {
     // cause weirdness outside this element.
     const shadowDOM = this.attachShadow({ mode: "open" });
 
-    this.innerStyle = document.createElement("style");
-    this.updateStyle();
-    shadowDOM.appendChild(this.innerStyle);
+    this.setDefaultStyle(shadowDOM);
 
-    const slot = document.createElement("span");
-    const slot_back = document.createElement("span");
-    const slot_front = document.createElement("span");
-    slot.setAttribute("class", "slot");
-    slot.style.display = "inline-block";
-    slot.style.position = "relative";
-    slot_front.setAttribute("class", "slot_front");
-    slot_front.style.display = "inline-block";
-    slot_front.style.position = "relative";
-    slot_back.setAttribute("class", "slot_back");
-    slot_back.style.display = "inline-block";
-    slot_back.style.position = "relative";
+    const slot       = this.createComponentElement("slot");
+    const slot_back  = this.createComponentElement("slot-back");
+    const slot_front = this.createComponentElement("slot-front");
     slot.appendChild(slot_back);
     slot.appendChild(slot_front);
 
-    const stick = document.createElement("span");
-    stick.setAttribute("class", "stick");
-    stick.style.display = "inline-block";
-    stick.style.position = "relative";
-
-    const ball = document.createElement("span");
-    ball.setAttribute("class", "ball");
-    ball.style.display = "inline-block";
-    ball.style.position = "absolute";
+    const stick = this.createComponentElement("stick");
+    const ball  = this.createComponentElement("ball");
+/*
+    ball.addEventListener("drag", (ev) => {
+// doesn't happen?
+      console.log(`CAN HAS DRAG: ${ev}`);
+    });
+ */
     stick.appendChild(ball);
 
     shadowDOM.appendChild(slot);
     shadowDOM.appendChild(stick);
+    this.updateStyle();
+
+    this.stick = stick;
   }
 
-  updateStyle() {
-    const existingStyle = window.getComputedStyle(this);
-
+  setDefaultStyle(el) {
+    this.innerStyle = document.createElement("style");
+    el.appendChild(this.innerStyle);
     this.innerStyle.textContent = `
       .ball {
-        background-color: red;
-        background-color: ${existingStyle.color};
+        background: radial-gradient(#000000, #ae0f0f);
         width: 500%;
         aspect-ratio: 1 / 1;
         border-radius: 50%;
@@ -74,11 +68,11 @@ class VirtualJoystickElement extends HTMLElement {
       .stick {
         width: 8%;
         height: 100%;
-        background-color: #505050;
+        background: radial-gradient(#000000, #777777); 
         border-radius: 0 0 80% 80%;
         color: #505050;
         transform-origin: 50% 100%;
-        rotate: 23deg;
+        /*rotate: 23deg; */
         z-index: 3;
       }
       .slot {
@@ -87,16 +81,16 @@ class VirtualJoystickElement extends HTMLElement {
         height: 20%;
         top: 100%;
       }
-      .slot_front {
-        background-color: #ffffff;
+      .slot-front {
+        background: linear-gradient(#000000, #777777);
         border: #a0a0a0 3px;
         height: 50%;
         bottom: -10%;
         width: 100%;
         z-index: 4;
       }
-      .slot_back {
-        background-color: #8f8f8f;
+      .slot-back {
+        background: linear-gradient(#777777, #000000);
         border-radius: 50vb 50vb 0 0;
         border: #808080 3px;
         height: 40%;
@@ -107,13 +101,105 @@ class VirtualJoystickElement extends HTMLElement {
     `;
   }
 
+  createComponentElement(elClass) {
+    const el = document.createElement("span");
+    el.setAttribute("class", elClass);
+    el.style.display = "inline-block";
+    el.style.position = "relative";
+
+    this.#subClasses.push(elClass);
+
+
+    return el;
+  }
+
+  updateStyle() {
+    const existingStyle = window.getComputedStyle(this);
+
+    // https://www.w3.org/TR/CSS2/cascade.html#cascading-order
+
+    // The idea here is this:
+    //   Style for sub props can be set using custom properties,
+    //  like this:
+    //  --[sub class]-[real css property] = [real css value]
+    // For example, to set a green border on the ball for the virtual
+    // joystick with id #joystick-1, you could do:
+    // #joystick-1 {
+    //   --ball-border: green 10px;
+    // }
+    // this is terribly brute force, but seems to be the best we can do?
+    var subStyle = "\n\n";
+    for(let subClass of this.#subClasses) {
+      subStyle += `.${subClass} {`;
+      // we can't use "of", below - we need all get possible css properties
+      for(let realProp in this.style) {
+        if(!isNaN(realProp)) {
+          // skip numeric ones because they aren't css properties.
+          // some of the non-numerics are also -not- css properties,
+          // but I'm guessing/hoping that even if something refers to
+          // one of them, though it'll go in the stylesheet, 
+          continue;
+        }
+
+        // convert camelCase to what's presumably the css version
+        // of the same thing:
+        realProp = realProp.replace(/[A-Z]/, "-$&").toLowerCase();
+
+        let propVal = existingStyle.getPropertyValue(
+            `--${subClass}-${realProp}`
+        );
+        if(propVal) {
+          subStyle += `\n    ${realProp}: ${propVal};`;
+        }
+      }
+      // and for the final evil trick, if there's (eg) just "--ball",
+      // that's a "background", because that's how we fill these elements:
+      let propVal = existingStyle.getPropertyValue(`--${subClass}`);
+      if(propVal) {
+          subStyle += `\n    background: ${propVal};`;
+      }
+      subStyle += "\n}\n";
+    }
+    this.innerStyle.textContent += subStyle;
+  }
+
   connectedCallback() {
-    console.log("Custom element added to page.");
+    // called when added to page
 
     this.style.minWidth  = "50px";
     this.style.minHeight = "30px";
     this.style.display   = 'inline-block';
     this.initGraphics();
+
+/*
+    this.addEventListener("mouseenter", (ev) => {
+      console.log("CAN HAS MOUSE ENTERED");
+      this.style.setProperty('background-color', 'orange');
+    });
+ */
+
+    var self = this;
+    let touchHandler = function(ev) {
+
+      console.log("CAN HAS TOUCH");
+      if(ev.targetTouches) {
+        if(ev.targetTouches.length >= 1) {
+          let bounds = self.getBoundingClientRect();
+          let touchX = ev.targetTouches.item(0).clientX;
+          if(touchX < bounds.x) touchX = bounds.x;
+          if(touchX > bounds.right) touchX = bounds.right;
+          // let's say we limit the joystick's swing to +- 45deg
+          // .. relative to the center.. hmm should be configurable.
+          let relativeX = touchX - (bounds.x + bounds.width/2);
+          self.stick.style.setProperty('rotate', `${45 * relativeX/bounds.width}deg`);
+          ev.preventDefault(); 
+        }
+      }
+    };
+
+    this.addEventListener("touchstart", touchHandler);
+    this.addEventListener("touchmove",  touchHandler);
+    this.addEventListener("touchend",   touchHandler);
 
     this.gamepad = new VirtualJoystickGamepad(this);
 /*
@@ -142,13 +228,12 @@ presumably this would fail, as well:
   }
 
   adoptedCallback() {
-    //console.log(`Joystick ${this} moved to new page.  whoa dude.`);
+    console.log(`Joystick ${this} moved to new page.  whoa dude.`);
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     console.log(`Joystick attribute ${name} has changed from '${oldValue}' to '${newValue}'`);
   }
-
 }
 
 window.customElements.define("virtual-joystick", VirtualJoystickElement);
